23ab12cebbe01c04ccb8a51579537349227166b3
connor parsons <parson67@uni.coventry.ac.uk>
2018-03-01 12:30:29 +0000

    got sensors working

--- subscriber3.py
+++ subscriber3.py
@@ -6,18 +6,31 @@ DB = 'danielnodedb.db'
 
 def on_connect( client, userdata, flags, rc ):
 #""" callback function for client connection """
-    client.subscribe( "davedata/outside/temp")
-    client.subscribe( "davedata/outside/humidity")
+    client.subscribe( "Danielnode/livestream/LinearAcceleration")
+    client.subscribe( "Danielnode/livestream/Gravity/z")
+    client.subscribe( "Danielnode/livestream/Accelerometer/z")     
+    client.subscribe( "Danielnode/livestream/LightIntensity/z")
+    client.subscribe( "Danielnode/livestream/noise/decibels/z") 
+     
 
 def on_message( client, userdata, msg ):
 #""" callback function for messages received """
     print( "Topic: {}, Message: {}".format(msg.topic, msg.payload) )
     
     sensor = None
-    if "temp" in msg.topic:
-        sensor = "temp"
-    elif "humidity" in msg.topic:
-        sensor = "humidity"
+    if "LinearAcceleration" in msg.topic: 
+        sensor = "LinearAcceleration"
+        
+  
+    elif "Accelerometer" in msg.topic:
+        sensor = "Accelerometer" 
+    elif "LightIntensity" in msg.topic:
+        sensor = "LightIntensity" 
+    elif "noise" in msg.topic:
+        sensor = "noise"
+    elif "Gravity" in msg.topic:
+        sensor = "Gravity"
+    # LinearAcceleration, Gravity, Accelerometer, LightIntensity, noise need to be changd to the sensor nodes in the DB.     
         
     value = float(msg.payload)
     now = datetime.datetime.now()
@@ -27,15 +40,15 @@ def on_message( client, userdata, msg ):
             cur.execute("""INSERT INTO sensordata VALUES(?, ?, ?);""", (sensor,value,now) )
           
     
-    
+#Need to work out if i actually need this    
 def value(self):
     with sql.connect(DB) as cur:
         cur = con.cursor(DB)
         cur.execute("""INSERT INTO sensordata VALUES(?, ?, ?);""", (sensor,value,now) )
         
-        if "light" in topic:
-            sensor = "light"
-    now = datetime.dateime.now().timetuple()
+        #if "light" in topic:
+            #sensor = "light"
+    #now = datetime.dateime.now().timetuple()
     #f = open("results.txt", "a")
     #print( "Topic: {}, Message: {}".format(msg.topic, msg.payload), file=f )
     #file.write("results.txt", "Danielnode/#")
@@ -46,3 +59,6 @@ client.on_connect = on_connect
 client.on_message = on_message
 client.connect( "iot.eclipse.org" ) # test broker
 client.loop_forever()
+
+#git status
+#git add sunscriber3
